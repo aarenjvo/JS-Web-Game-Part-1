d983fbf5021bb8222c1b5785d9925411a22041dc
Aaren Vo <aarenjvo@gmail.com>
2024-01-16 09:31:04 -0800

    added newImages, newItems, and tile functions

---
+++ examples.js
@@ -0,0 +1,11 @@
+let sword = document.createElement('img')
+sword.src = 'assets/sword.png'
+sword.position = 'fixed'
+sword.left = '500px'
+sword.bottom = '405px'
+document.body.append(sword)
+
+sword.addEventListener('click', function(){
+    sword.remove()
+})
+
